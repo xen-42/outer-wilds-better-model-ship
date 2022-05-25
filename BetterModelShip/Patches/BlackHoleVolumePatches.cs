2dc882f5627e332d5ca2ffd2f767ef1aae8d6cb2
Nick <nicholasconnors@hotmail.ca>
2022-05-25 16:33:57 -0400

    Recall ship when entering black hole

---
+++ BetterModelShip/Patches/BlackHoleVolumePatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BetterModelShip.Patches
+{
+    [HarmonyPatch(typeof(BlackHoleVolume))]
+    public static class BlackHoleVolumePatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(BlackHoleVolume.VanishModelRocketShip))]
+        public static bool BlackHoleVolume_VanishModelRocketShip(BlackHoleVolume __instance)
+        {
+            GameObject.FindObjectOfType<RemoteFlightConsole>().RespawnModelShip(true);
+
+            return false;
+        }
+    }
+}
