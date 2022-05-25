0d3408ca7ee48886d1256d4a7feb00ecfda279df
JohnCorby <williamforgames6@gmail.com>
2022-05-25 12:29:04 -0700

    remove da funny RegisterSectorDetector since it already happens

--- BetterModelShip/BetterModelShip.cs
+++ BetterModelShip/BetterModelShip.cs
@@ -78,8 +78,6 @@ namespace BetterModelShip
             attachGO.transform.localPosition = Vector3.zero;
             attachGO.transform.rotation = Quaternion.Euler(270, 0, 0);
             _playerAttachPoint = attachGO.AddComponent<PlayerAttachPoint>();
-
-            SectorManager.RegisterSectorDetector(detector.GetComponent<SectorDetector>());
         }
 
         private void Init()
